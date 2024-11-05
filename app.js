88a8c238da889cdb76f8ff92c60061e20ebc4a69
Ranjitha R Bhat <114816434+ranjitharbhat@users.noreply.github.com>
2024-11-05 16:49:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const urlRouter = require('./routes/urlRoutes');
 const app = express();
 
-app.use(express.json()); // For parsing JSON
+app.use(express.json()); 
 
-// Routes
 app.use('/api/v1/urls', urlRouter);
 
 module.exports = app;
